bab11fd55b047dcd6c2c461bf9de00e8c4df1a6a
Daniel Gao <danielgao341@gmail.com>
2023-12-21 15:05:47 -0500

    blog changes

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -50,15 +50,6 @@ const Contact = () => {
                   </svg>
                 </p>
               </a>
-              <span class="text-gray-400 mr-3 inline-flex items-center lg:ml-auto md:ml-0 ml-auto leading-none text-sm pr-3 py-1 border-r-2 border-gray-200">
-                <svg class="w-4 h-4 mr-1"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round">  
-                  <path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z" />
-                </svg>
-                24
-              </span>
-              <span class="text-gray-400 inline-flex items-center leading-none text-sm">
-
-              </span>
             </div>
             
             
