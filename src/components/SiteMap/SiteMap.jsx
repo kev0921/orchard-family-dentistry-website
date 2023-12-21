d2ac77852595cc189b770f6bff7ad10c138d372e
kev0921 <kevin.hu0921@gmail.com>
2023-12-21 01:03:30 -0500

    site map looks better

--- src/components/SiteMap/SiteMap.jsx
+++ src/components/SiteMap/SiteMap.jsx
@@ -1,203 +1,232 @@
 import React from "react";
 
 const SiteMap = () => {
+  return (
+    <div>
+      <h1>Website Sitemap</h1>
+      <ul>
+        <li>
+          <strong>Home:</strong>{" "}
+          <a href="https://orchardfamilydentistry.ca/" target="_blank" rel="noopener noreferrer">
+            https://orchardfamilydentistry.ca/
+          </a>
+        </li>
 
-return (
-<div>
-  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-    <url>
-      <loc>https://orchardfamilydentistry.ca/</loc>
-      <changefreq>weekly</changefreq>
-      <priority>1.0</priority>
-    </url>
+        <li>
+          <strong>Patient Information:</strong>
+          <ul>
+            <li>
+              <a href="https://orchardfamilydentistry.ca/patientinformation" target="_blank" rel="noopener noreferrer">
+                Overview
+              </a>
+            </li>
+            <li>
+              <a href="https://orchardfamilydentistry.ca/patientinformation/scheduling" target="_blank" rel="noopener noreferrer">
+                Scheduling
+              </a>
+            </li>
+            <li>
+              <a href="https://orchardfamilydentistry.ca/patientinformation/studentdiscounts" target="_blank" rel="noopener noreferrer">
+                Student Discounts
+              </a>
+            </li>
+            <li>
+              <a href="https://orchardfamilydentistry.ca/patientinformation/patienteducation" target="_blank" rel="noopener noreferrer">
+                Patient Education
+              </a>
+            </li>
+            <li>
+              <a href="https://orchardfamilydentistry.ca/patientinformation/patienteducation/digitalxrays" target="_blank" rel="noopener noreferrer">
+                Digital X-Rays
+              </a>
+            </li>
+            <li>
+              <a href="https://orchardfamilydentistry.ca/patientinformation/patienteducation/intraoralcamera" target="_blank" rel="noopener noreferrer">
+                Intraoral Camera
+              </a>
+            </li>
+            <li>
+              <a href="https://orchardfamilydentistry.ca/patientinformation/patienteducation/localanesthesia" target="_blank" rel="noopener noreferrer">
+                Local Anesthesia
+              </a>
+            </li>
+            <li>
+              <a href="https://orchardfamilydentistry.ca/patientinformation/patienteducation/patientsafety" target="_blank" rel="noopener noreferrer">
+                Patient Safety
+              </a>
+            </li>
+            <li>
+              <a href="https://orchardfamilydentistry.ca/patientinformation/patienteducation/toothdecay" target="_blank" rel="noopener noreferrer">
+                Tooth Decay
+              </a>
+            </li>
+          </ul>
+        </li>
 
-    <url>
-      <loc>https://orchardfamilydentistry.ca/patientinformation</loc>
-      <changefreq>monthly</changefreq>
-      <priority>0.8</priority>
-    </url>
-    <url>
-      <loc>https://orchardfamilydentistry.ca/patientinformation/scheduling</loc>
-      <changefreq>monthly</changefreq>
-      <priority>0.6</priority>
-    </url>
-    <url>
-      <loc>https://orchardfamilydentistry.ca/patientinformation/studentdiscounts</loc>
-      <changefreq>monthly</changefreq>
-      <priority>0.6</priority>
-    </url>
-    <url>
-      <loc>https://orchardfamilydentistry.ca/patientinformation/patienteducation</loc>
-      <changefreq>monthly</changefreq>
-      <priority>0.6</priority>
-    </url>
-    <url>
-      <loc>https://orchardfamilydentistry.ca/patientinformation/patienteducation/digitalxrays</loc>
-      <changefreq>monthly</changefreq>
-      <priority>0.6</priority>
-    </url>
-    <url>
-      <loc>https://orchardfamilydentistry.ca/patientinformation/patienteducation/intraoralcamera</loc>
-      <changefreq>monthly</changefreq>
-      <priority>0.6</priority>
-    </url>
-    <url>
-      <loc>https://orchardfamilydentistry.ca/patientinformation/patienteducation/localanesthesia</loc>
-      <changefreq>monthly</changefreq>
-      <priority>0.6</priority>
-    </url>
-    <url>
-      <loc>https://orchardfamilydentistry.ca/patientinformation/patienteducation/patientsafety</loc>
-      <changefreq>monthly</changefreq>
-      <priority>0.6</priority>
-    </url>
-    <url>
-      <loc>https://orchardfamilydentistry.ca/patientinformation/patienteducation/toothdecay</loc>
-      <changefreq>monthly</changefreq>
-      <priority>0.6</priority>
-    </url>
+        <li>
+          <strong>Special Offers:</strong>{" "}
+          <a href="https://orchardfamilydentistry.ca/offers" target="_blank" rel="noopener noreferrer">
+            View Offers
+          </a>
+        </li>
 
-    <url>
-      <loc>https://orchardfamilydentistry.ca/offers</loc>
-      <changefreq>monthly</changefreq>
-      <priority>0.7</priority>
-    </url>
+        <li>
+          <strong>Meet Us:</strong>{" "}
+          <a href="https://orchardfamilydentistry.ca/meetus" target="_blank" rel="noopener noreferrer">
+            Meet Our Team
+          </a>
+        </li>
 
-    <url>
-      <loc>https://orchardfamilydentistry.ca/meetus</loc>
-      <changefreq>monthly</changefreq>
-      <priority>0.7</priority>
-    </url>
+        <li>
+          <strong>Dental Services:</strong>
+          <ul>
+            <li>
+              <a href="https://orchardfamilydentistry.ca/dentalservices" target="_blank" rel="noopener noreferrer">
+                Overview
+              </a>
+            </li>
+            <li>
+              <a href="https://orchardfamilydentistry.ca/dentalservices/emergencydentistry" target="_blank" rel="noopener noreferrer">
+                Emergency Dentistry
+              </a>
+            </li>
+            <li>
+              <a href="https://orchardfamilydentistry.ca/dentalservices/generaldentistry" target="_blank" rel="noopener noreferrer">
+                General Dentistry
+              </a>
+              <ul>
+                <li>
+                  <a href="https://orchardfamilydentistry.ca/dentalservices/generaldentistry/digitalxrays2" target="_blank" rel="noopener noreferrer">
+                    Digital X-Rays
+                  </a>
+                </li>
+                <li>
+                  <a href="https://orchardfamilydentistry.ca/dentalservices/generaldentistry/sedationdentistry" target="_blank" rel="noopener noreferrer">
+                    Sedation Dentistry
+                  </a>
+                </li>
+                <li>
+                  <a href="https://orchardfamilydentistry.ca/dentalservices/generaldentistry/wisdomteethremoval" target="_blank" rel="noopener noreferrer">
+                    Wisdom Teeth Removal
+                  </a>
+                </li>
+                <li>
+                  <a href="https://orchardfamilydentistry.ca/dentalservices/generaldentistry/rootcanaltherapy" target="_blank" rel="noopener noreferrer">
+                    Root Canal Therapy
+                  </a>
+                </li>
+              </ul>
+            </li>
+            <li>
+              <a href="https://orchardfamilydentistry.ca/dentalservices/restorativedentistry" target="_blank" rel="noopener noreferrer">
+                Restorative Dentistry
+              </a>
+              <ul>
+                <li>
+                  <a href="https://orchardfamilydentistry.ca/dentalservices/restorativedentistry/crowns" target="_blank" rel="noopener noreferrer">
+                    Crowns
+                  </a>
+                </li>
+                <li>
+                  <a href="https://orchardfamilydentistry.ca/dentalservices/restorativedentistry/dentures" target="_blank" rel="noopener noreferrer">
+                    Dentures
+                  </a>
+                </li>
+                <li>
+                  <a href="https://orchardfamilydentistry.ca/dentalservices/restorativedentistry/dentalimplants" target="_blank" rel="noopener noreferrer">
+                    Dental Implants
+                  </a>
+                </li>
+              </ul>
+            </li>
+            <li>
+              <a href="https://orchardfamilydentistry.ca/dentalservices/cosmeticdentist" target="_blank" rel="noopener noreferrer">
+                Cosmetic Dentistry
+              </a>
+              <ul>
+                <li>
+                  <a href="https://orchardfamilydentistry.ca/dentalservices/cosmeticdentist/toothcoloureddentistry" target="_blank" rel="noopener noreferrer">
+                    Tooth-Coloured Dentistry
+                  </a>
+                </li>
+                <li>
+                  <a href="https://orchardfamilydentistry.ca/dentalservices/cosmeticdentist/dentalfillings" target="_blank" rel="noopener noreferrer">
+                    Dental Fillings
+                  </a>
+                </li>
+                <li>
+                  <a href="https://orchardfamilydentistry.ca/dentalservices/cosmeticdentist/porcelainveneers" target="_blank" rel="noopener noreferrer">
+                    Porcelain Veneers
+                  </a>
+                </li>
+                <li>
+                  <a href="https://orchardfamilydentistry.ca/dentalservices/cosmeticdentist/mercuryfreedentistry" target="_blank" rel="noopener noreferrer">
+                    Mercury-Free Dentistry
+                  </a>
+                </li>
+                <li>
+                  <a href="https://orchardfamilydentistry.ca/dentalservices/cosmeticdentist/teethwhitening" target="_blank" rel="noopener noreferrer">
+                    Teeth Whitening
+                  </a>
+                </li>
+              </ul>
+            </li>
+            <li>
+              <a href="https://orchardfamilydentistry.ca/dentalservices/preventativecare" target="_blank" rel="noopener noreferrer">
+                Preventative Care
+              </a>
+            </li>
+            <li>
+              <a href="https://orchardfamilydentistry.ca/dentalservices/invisaline" target="_blank" rel="noopener noreferrer">
+                Invisalign
+              </a>
+            </li>
+          </ul>
+        </li>
 
-    <url>
-      <loc>https://orchardfamilydentistry.ca/dentalservices</loc>
-      <changefreq>monthly</changefreq>
-      <priority>0.8</priority>
-    </url>
-    <url>
-      <loc>https://orchardfamilydentistry.ca/dentalservices/emergencydentistry</loc>
-      <changefreq>monthly</changefreq>
-      <priority>0.6</priority>
-    </url>
-    <url>
-      <loc>https://orchardfamilydentistry.ca/dentalservices/generaldentistry</loc>
-      <changefreq>monthly</changefreq>
-      <priority>0.8</priority>
-    </url>
-    <url>
-      <loc>https://orchardfamilydentistry.ca/dentalservices/generaldentistry/digitalxrays2</loc>
-      <changefreq>monthly</changefreq>
-      <priority>0.6</priority>
-    </url>
-    <url>
-      <loc>https://orchardfamilydentistry.ca/dentalservices/generaldentistry/sedationdentistry</loc>
-      <changefreq>monthly</changefreq>
-      <priority>0.6</priority>
-    </url>
-    <url>
-      <loc>https://orchardfamilydentistry.ca/dentalservices/generaldentistry/wisdomteethremoval</loc>
-      <changefreq>monthly</changefreq>
-      <priority>0.6</priority>
-    </url>
-    <url>
-      <loc>https://orchardfamilydentistry.ca/dentalservices/generaldentistry/rootcanaltherapy</loc>
-      <changefreq>monthly</changefreq>
-      <priority>0.6</priority>
-    </url>
-    <url>
-      <loc>https://orchardfamilydentistry.ca/dentalservices/restorativedentistry</loc>
-      <changefreq>monthly</changefreq>
-      <priority>0.8</priority>
-    </url>
-    <url>
-      <loc>https://orchardfamilydentistry.ca/dentalservices/restorativedentistry/crowns</loc>
-      <changefreq>monthly</changefreq>
-      <priority>0.6</priority>
-    </url>
-    <url>
-      <loc>https://orchardfamilydentistry.ca/dentalservices/restorativedentistry/dentures</loc>
-      <changefreq>monthly</changefreq>
-      <priority>0.6</priority>
-    </url>
-    <url>
-      <loc>https://orchardfamilydentistry.ca/dentalservices/restorativedentistry/dentalimplants</loc>
-      <changefreq>monthly</changefreq>
-      <priority>0.6</priority>
-    </url>
-    <url>
-      <loc>https://orchardfamilydentistry.ca/dentalservices/cosmeticdentist</loc>
-      <changefreq>monthly</changefreq>
-      <priority>0.8</priority>
-    </url>
-    <url>
-      <loc>https://orchardfamilydentistry.ca/dentalservices/cosmeticdentist/toothcoloureddentistry</loc>
-      <changefreq>monthly</changefreq>
-      <priority>0.6</priority>
-    </url>
-    <url>
-      <loc>https://orchardfamilydentistry.ca/dentalservices/cosmeticdentist/dentalfillings</loc>
-      <changefreq>monthly</changefreq>
-      <priority>0.6</priority>
-    </url>
-    <url>
-      <loc>https://orchardfamilydentistry.ca/dentalservices/cosmeticdentist/porcelainveneers</loc>
-      <changefreq>monthly</changefreq>
-      <priority>0.6</priority>
-    </url>
-    <url>
-      <loc>https://orchardfamilydentistry.ca/dentalservices/cosmeticdentist/mercuryfreedentistry</loc>
-      <changefreq>monthly</changefreq>
-      <priority>0.6</priority>
-    </url>
-    <url>
-      <loc>https://orchardfamilydentistry.ca/dentalservices/cosmeticdentist/teethwhitening</loc>
-      <changefreq>monthly</changefreq>
-      <priority>0.6</priority>
-    </url>
-    <url>
-      <loc>https://orchardfamilydentistry.ca/dentalservices/preventativecare</loc>
-      <changefreq>monthly</changefreq>
-      <priority>0.7</priority>
-    </url>
-    <url>
-      <loc>https://orchardfamilydentistry.ca/dentalservices/invisaline</loc>
-      <changefreq>monthly</changefreq>
-      <priority>0.7</priority>
-    </url>
-    
-    <url>
-      <loc>https://orchardfamilydentistry.ca/contactus</loc>
-      <changefreq>monthly</changefreq>
-      <priority>0.7</priority>
-    </url>
-    <url>
-      <loc>https://orchardfamilydentistry.ca/blog</loc>
-      <changefreq>weekly</changefreq>
-      <priority>0.9</priority>
-    </url>
-    <url>
-      <loc>https://orchardfamilydentistry.ca/appointment</loc>
-      <changefreq>weekly</changefreq>
-      <priority>0.9</priority>
-    </url>
-    <url>
-      <loc>https://orchardfamilydentistry.ca/thankyou</loc>
-      <changefreq>monthly</changefreq>
-      <priority>0.7</priority>
-    </url>
-    <url>
-      <loc>https://orchardfamilydentistry.ca/privacypolicy</loc>
-      <changefreq>monthly</changefreq>
-      <priority>0.7</priority>
-    </url>
-    <url>
-      <loc>https://orchardfamilydentistry.ca/404</loc>
-      <changefreq>monthly</changefreq>
-      <priority>0.5</priority>
-    </url>
+        <li>
+          <strong>Contact Us:</strong>{" "}
+          <a href="https://orchardfamilydentistry.ca/contactus" target="_blank" rel="noopener noreferrer">
+            Contact Information
+          </a>
+        </li>
 
-  </urlset>
-</div>
+        <li>
+          <strong>Blog:</strong>{" "}
+          <a href="https://orchardfamilydentistry.ca/blog" target="_blank" rel="noopener noreferrer">
+            Read Our Blog
+          </a>
+        </li>
+
+        <li>
+          <strong>Appointment:</strong>{" "}
+          <a href="https://orchardfamilydentistry.ca/appointment" target="_blank" rel="noopener noreferrer">
+            Schedule an Appointment
+          </a>
+        </li>
+
+        <li>
+          <strong>Thank You:</strong>{" "}
+          <a href="https://orchardfamilydentistry.ca/thankyou" target="_blank" rel="noopener noreferrer">
+            Thank You Page
+          </a>
+        </li>
+
+        <li>
+          <strong>Privacy Policy:</strong>{" "}
+          <a href="https://orchardfamilydentistry.ca/privacypolicy" target="_blank" rel="noopener noreferrer">
+            Privacy Policy
+          </a>
+        </li>
+
+        <li>
+          <strong>404 Error:</strong>{" "}
+          <a href="https://orchardfamilydentistry.ca/404" target="_blank" rel="noopener noreferrer">
+            404 Error Page
+          </a>
+        </li>
+      </ul>
+    </div>
   );
 };
 
