44376e0a293567707d00cf143fc0f7038778d42a
kev0921 <kevin.hu0921@gmail.com>
2024-01-10 00:45:06 -0500

    fixed veneers page

--- src/components/DentalServices/CosmeticDentist/PorcelainVeneers.jsx
+++ src/components/DentalServices/CosmeticDentist/PorcelainVeneers.jsx
@@ -1,43 +1,83 @@
-import React, { useState, useEffect } from "react";
-import Orchard2 from "../../../images/porcelainveneers.jpg";
+import React from "react";
 
-const PorcelainVeneers = () => {
-  const [,setAnimate] = useState(false);
-
-  useEffect(() => {
-    setAnimate(true);
-  },[]);
-
+const TmdJawPain = () => {
   return (
-    <section class="bg-white">
-        <div class="container px-6 py-5 mx-auto mb-12">
-            <div class="mt-8 lg:-mx-6 lg:flex lg:items-center">
-                
-                <img class="object-cover w-full lg:mx-6 lg:w-2/4 rounded-xl h-78 lg:h-200" src={Orchard2}alt=""/>
-    
-                <div class="mt-6 lg:w-1/2 lg:mt-0 lg:mx-6">
+    <div className="bg-cover">
+      <div className="bg-sky-300 py-4 text-white text-center">
+        <p className="text-2xl font-semibold">
+          Book Your Dental Appointment Today! Call: 905-315-9937
+        </p>
+      </div>
+      <div className="px-10 md:px-0 md:w-3/5 py-16 md:mx-auto">
+        <div className="md:px-0">
+        <section className="mb-16">
+          <h1 className="text-5xl md:text-6xl font-semibold text-left text-blue-900 ">
+          Veneers
+          </h1>
+          <p className="mt-4 text-2xl text-left text-blue-900">
+          Unsatisfied with how your teeth look? Teeth that are chipped, stained or misshapen can take away from an otherwise beautiful smile. Veneers can rejuvenate your lackluster teeth & transform your smile.
+          </p>
+          <p className="mt-4 text-2xl text-left text-blue-900">
+          Having a great smile can make a huge difference in the way you look & feel. We offer a complete range of cosmetic dentistry treatments to create the confident smile you’ve always wanted. We begin with a detailed smile assessment, discussing with you which aspects you like & dislike about your smile. Once our assessment is complete, we will discuss with you treatment options to help your smile look more natural, healthy & youthful.
+          </p>
+        </section>
 
-                    <p class="text-lg text-blue-300">
-                    EXCELLENT ALTERNATIVE TO CROWNS
-                    </p>
-    
-                    <h2 class="block mt-4 text-3xl md:text-5xl font-semibold text-gray-800">
-                    Porcelain Veneers
-                    </h2>
-    
-                    <h2 class="block mt-2 md:mt-4 md:text-3xl md:text-3xl font-semibold text-gray-800">
-                    Porcelain veneers are an excellent alternative to crowns & can perfect your smile by masking stains, hiding chips, correcting misalignment & creating uniformity.                    
-                    </h2>
+        <section className="mb-16 border py-10 px-10 bg-cover bg-blue-100">
+          <h2 className="text-5xl font-semibold text-left text-blue-900 ">
+          What Are Dental Veneers?          
+          </h2>
+          <p className="mt-4 text-2xl text-left text-blue-900">
+          Veneers are the most conservative restorations we offer. They consist of a thin strip of porcelain that covers the front side of a tooth. We use porcelain because we can change the color to closely match your natural teeth. Unlike crowns, which cover the entirety of a tooth, veneers require less enamel removal, so most of your original tooth structure stays intact. Rather than replacing your whole tooth, a veneer gives your tooth a “new face.” Because they are minimally invasive, dental veneers are essential tools of cosmetic dentistry.
+          </p>
+          <p className="mt-4 text-2xl text-left text-blue-900">
+          Getting a veneer requires at least two appointments. At the first appointment, the dentist prepares your tooth so a veneer can be placed. A couple weeks later, they will permanently attach the veneer to your tooth.
+          </p>
+        </section>
 
-                    <p class="mt-3 text-gray-500 md:text-lg">
-                    Veneers can be placed over existing teeth without the removal of any form of tooth structure. Therefore, anesthesia & temporaries are not required. This no-prep technique is perfect for stained, chipped, misaligned or spaced teeth. Bonding porcelain to enamel creates a solid, secure bond, meaning that you’ll have a safe, relatively easy, attractive & long-term solution with porcelain veneers.                    
-                    </p>    
+        <section className="mb-16">
+          <div>
+            <h2 className="text-5xl font-semibold text-left text-blue-900">
+              Procedure Overview
+            </h2>
+            <ul className="list-disc pl-6">
+              <li>
+                <p className="mt-4 text-2xl text-left text-blue-900">
+                  A dental veneer procedure begins with the dentist removing a very small amount of your front tooth enamel (usually less than one millimeter). Depending on how much enamel needs to be removed, this process may not require local anesthesia. Veneers bond more easily with the outer tooth structure than the inner tooth structure, so the dentist wants to remove as little as possible. They use a rotating device known as a dental bur to gently shave down some of the tooth’s surface.
+                </p>
+              </li>
+              <li>
+                <p className="mt-4 text-2xl text-left text-blue-900">
+                  After the tooth is shaved down, we need to take an impression (or mold) of your teeth. We send this impression to a lab so they can make a veneer that comfortably fits your smile. To expose more of your tooth for the impression, the dentist uses a small retraction cord to move the gums away from it. This is temporary; your gum line returns to its original position once the impression is complete. The final step is to bite into a tray filled with paste to a make a mold of your teeth. This takes a few minutes to set, so be patient!
+                </p>
+              </li>
+              <li>
+                <p className="mt-4 text-2xl text-left text-blue-900">
+                  When the mold is ready, we make any final adjustments & send it to a dental lab. It takes a couple of weeks to receive your permanent veneer from the lab. In the meantime, you may receive a temporary veneer to cover the prepared tooth. This is not necessary in all cases. Temporaries are only needed if the dentist must remove a large amount of enamel, which can make your tooth more sensitive or prone to damage.
+                </p>
+              </li>
+              <li>
+                <p className="mt-4 text-2xl text-left text-blue-900">
+                  Once any last-minute trimming & refitting is finished, the dentist coats your tooth with bonding cement & attaches the veneer to your tooth. Then they use a special blue light to harden the cement & secure the veneer. After one final check to make sure the fit is still snug & comfy, you’re good to go!
+                </p>
+              </li>
+            </ul>
+          </div>
+        </section>
 
-                </div>
-            </div>
+        <section className="">
+          <p className="mt-4 text-2xl text-left text-blue-900">
+          You will be given special instructions on how to care for your veneer, including a list of foods to avoid. You may also need a night guard if you grind your teeth at night. Carefully following any directions or warnings will ensure the longest lifespan for your veneer, which can last over ten years.          
+          </p>
+          <p className="mt-4 text-2xl text-left text-blue-900">
+          Call us to find out how veneers can beautify your smile.         
+           </p>
+        </section>
+
+
         </div>
-    </section>
+      </div>
+    </div>
   );
 };
 
-export default PorcelainVeneers;
+export default TmdJawPain;
